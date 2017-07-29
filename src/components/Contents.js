5e1eaddf9fb9bffa2f80ef8e83360e91b549ba3e
Harley Turan <harley@cloudflare.com>
2017-07-29 15:44:36 +0100

    Generate Contents animation from base color

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -9,14 +9,14 @@ const listStyle = {
 
 const linkStyle = {
   color: '#666',
-  fontSize: '30px',
-  lineHeight: '70px',
+  fontSize: '3vw',
+  lineHeight: '6vw',
   textDecoration: 'none'
 };
 
 export default props =>
   <div style={{ paddingLeft: '50px' }}>
-    <h1 style={{ fontWeight: 400, margin: 0, paddingTop: '50px' }}>
+    <h1 style={{ fontSize: '3vw', fontWeight: 400, margin: 0, paddingTop: '50px' }}>
       Interaction of Color
     </h1>
 
@@ -33,47 +33,48 @@ export default props =>
       </li>
     </ul>
 
-    <IntensityAnimation />
+    <IntensityAnimation baseColor="red" />
   </div>;
 
-const reds = [
-  {
-    color: chroma.hsl(8, 0.78, 0.69),
-    zIndex: Math.floor(Math.random() * 10)
-  },
-  {
-    color: chroma.hsl(9, 0.75, 0.55),
-    zIndex: Math.floor(Math.random() * 10)
-  },
-  {
-    color: chroma.hsl(356, 0.7, 0.61),
-    zIndex: Math.floor(Math.random() * 10)
-  },
-  {
-    color: chroma.hsl(357, 0.65, 0.45),
-    zIndex: Math.floor(Math.random() * 10)
-  },
-  {
-    color: chroma.hsl(357, 0.72, 0.72),
-    zIndex: Math.floor(Math.random() * 10)
-  },
-  {
-    color: chroma.hsl(351, 0.42, 0.52),
-    zIndex: Math.floor(Math.random() * 10)
-  },
-  {
-    color: chroma.hsl(9, 0.78, 0.56),
-    zIndex: Math.floor(Math.random() * 10)
-  },
-  {
-    color: chroma.hsl(344, 0.69, 0.43),
-    zIndex: Math.floor(Math.random() * 10)
-  },
-];
+// const reds = [
+//   chroma.hsl(8, 0.78, 0.69),
+//   chroma.hsl(9, 0.75, 0.55),
+//   chroma.hsl(356, 0.7, 0.61),
+//   chroma.hsl(357, 0.65, 0.45),
+//   chroma.hsl(357, 0.72, 0.72),
+//   chroma.hsl(351, 0.42, 0.52),
+//   chroma.hsl(9, 0.78, 0.56),
+//   chroma.hsl(344, 0.69, 0.43)
+// ];
+
+const generateHue = baseColor => {
+  const baseHue = chroma(baseColor).hsl()[0];
+  return (baseHue + 10 - Math.ceil(Math.random() * 20)) % 359;
+};
 
 class IntensityAnimation extends Component {
+  constructor(props) {
+    super();
+
+    this.colors = [];
+
+    const max = 0.7;
+    const min = 0.45;
+
+    for (let i = 0; i < 30; i++) {
+      this.colors.push({
+        color: chroma.hsl(
+          generateHue(props.baseColor),
+          Math.random() * (max - min) + min,
+          Math.random() * (max - min) + min
+        ),
+        zIndex: Math.floor(Math.random() * 10)
+      });
+    }
+  }
+
   componentDidMount() {
-    this.interval = setInterval(() => this.forceUpdate(), 7000);
+    this.interval = setInterval(() => this.forceUpdate(), 9000);
 
     setTimeout(() => this.forceUpdate(), 0);
   }
@@ -97,7 +98,7 @@ class IntensityAnimation extends Component {
           justifyContent: 'center'
         }}
       >
-        {reds.map(({color, zIndex}, index) =>
+        {this.colors.map(({ color, zIndex }, index) =>
           <div
             key={index}
             style={{
@@ -108,8 +109,9 @@ class IntensityAnimation extends Component {
               top: 0,
               zIndex,
               backgroundColor: color,
-              transform: `rotate(${10 * Math.random() - 5}deg) translate(${100 * Math.random()}px, ${ 60 * Math.random() - 20}px)`,
-              transition: 'transform 5s'
+              transform: `rotate(${10 * Math.random() - 5}deg) translate(${100 *
+                Math.random()}px, ${60 * Math.random() - 20}px)`,
+              transition: 'transform 8s'
             }}
           />
         )}
